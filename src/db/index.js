69535b65a12ad6ec5b781fb2f8f692c7ec018c6f
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-02 23:47:43 +0530

    Connection to Database Done.

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async()=>{
+    try {
+        const connectionResp = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\nMongoDB connected HOST :${connectionResp.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection error",error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
