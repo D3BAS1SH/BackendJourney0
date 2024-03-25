4994d8f70fe27df4f0217e4a2a3f07e1b893a96e
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-26 00:44:17 +0530

    cloudinary file added to handle file upload in cloudinary.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2} from 'cloudinary'
+import fs from 'fs'
+
+          
+v2.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath){
+            return null
+        }
+        //upload the file on cloudinary
+        const response = await v2.uploader.upload(localFilePath,{resource_type:"auto"})
+        console.log('File upload successful.',response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export{uploadOnCloudinary}
