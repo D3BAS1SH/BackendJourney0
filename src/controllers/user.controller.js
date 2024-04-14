c2a7a903c26a635453dc71998dc2ba0289264e60
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-15 02:13:51 +0530

    Checking if the fields are empty. Done!

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,22 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/ApiError.js'
 
 const registerUser = asyncHandler( async (req,res)=>{
     // Get user details from frontend
     const {fullname,email,username,password} = req.body
     console.log(email,fullname);
     // validation - (not empty)
-    
+    /* if(fullname===""){
+        throw new ApiError(400,'full name is required.');
+    } */
+    if(
+        [fullname,email,username,password].some((fields)=>fields?.trim()==="")
+    ){
+        throw new ApiError(400,'All fields are required.');
+    }
     // Check if user already exists by username or email
+
+    
 
     // check for images, check for avatar
 
