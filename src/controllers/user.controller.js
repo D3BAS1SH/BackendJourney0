35556cda2b21d6a8b38b0d603c5d089f5b2b3d8f
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-15 02:55:14 +0530

    Avatar and coverImage loading and checking if the files are uploaded properly or not.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req,res)=>{
     // Get user details from frontend
@@ -16,9 +17,21 @@ const registerUser = asyncHandler( async (req,res)=>{
     }
     // Check if user already exists by username or email
 
-    
+    const existedUser = User.findOne({
+        $or:[{ username },{ email }]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User Already exits.")
+    }
 
     // check for images, check for avatar
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar File is required.")
+    }
 
     // upload them to cloudinary, avatar
 
