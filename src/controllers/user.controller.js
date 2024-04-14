9c0d839a74d824c0565cdf7be401ad1048cc4f91
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-15 00:08:19 +0530

    Register controller handling Initiation.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,23 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler( async (req,res)=>{
+    // Get user details from frontend
+    req.body
+    // validation - (not empty)
+    
+    // Check if user already exists by username or email
+
+    // check for images, check for avatar
+
+    // upload them to cloudinary, avatar
+
+    // create user object - create entry in DB.
+
+    // remove password and refresh token field from response.
+
+    // check for user creation.
+
+    // return response
     res.status(200).json({
         message:"Hello friends"
     })
