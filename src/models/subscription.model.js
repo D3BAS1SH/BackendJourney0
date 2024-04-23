a1bebb8236c80c4337d07e958141afe745e163bc
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-23 20:49:29 +0530

    Added subscription schema

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,16 @@
 import mongoose,{Schema, model} from "mongoose";
 
-const SubscriptionSchema= new Schema({})
+const SubscriptionSchema= new Schema({
+    subscribers:{
+        //One who is subscribing
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        //one who is subscribed.
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
 
 export const Subscription = mongoose.model("Subscription",SubscriptionSchema)
